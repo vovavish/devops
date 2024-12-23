9d93502ebb74346a036628b3828f77afde547f15
vovavish <vovavishniakov856@gmail.com>
2024-12-24 01:19:54 +0300

    first commit

---
+++ AddModule.test.js
@@ -0,0 +1,5 @@
+const { addNumbers } = require('./AddModule');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(addNumbers(1, 2)).toBe(3);
+});
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Вишняков Владимир ИДБ-21-12');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
